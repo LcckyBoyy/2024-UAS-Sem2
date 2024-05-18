42134a87714cb171d57c8cb4c484991908feb8ea
Will  <speedbolt1004@gmail.com>
2024-05-18 16:38:39 +0700

    mainView

---
+++ resources/views/mainView.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rasa Sulawesi</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <div class='testStylingForHomeView'>
+        <section class='height:1000pt bgc:'>
+
+        </section>
+    </div>
+
+</body>
+</html>
